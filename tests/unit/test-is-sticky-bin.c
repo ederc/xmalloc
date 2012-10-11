e62859dcc247d7e7b155b9bd84a8274e96b8a390
Christian Eder <ederc@mathematik.uni-kl.de>
2012-10-11 09:34:26 +0200

    adds unit test for sticky bins

---
+++ tests/unit/test-is-sticky-bin.c
@@ -0,0 +1,24 @@
+/**
+ * \file   test-is-sticky-bin.c
+ * \Author Christian Eder ( ederc@mathematik.uni-kl.de )
+ * \date   October 2012
+ * \brief  Unit test for sticky bins for xmalloc.
+ *         This file is part of XMALLOC, licensed under the GNU General
+ *         Public License version 3. See COPYING for more information.
+ */
+
+#include <stdio.h>
+#include "xmalloc-config.h"
+#include "xmalloc.h"
+
+int main() {
+  // get a non-sticky bin
+  xBin bin = xMalloc(sizeof(xBinType));
+  __XMALLOC_ASSERT(0 == xIsStickyBin(bin));
+  
+  // get the corresponding sticky bin
+  xBin sBin = xGetStickyBinOfBin(bin);
+  __XMALLOC_ASSERT(1 == xIsStickyBin(sBin));
+  
+  return 0;
+}
